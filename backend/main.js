fb54bc95e69282e3f44a8cf471ffb88c4433a064
David Cohen <deiv464@gmail.com>
2024-06-24 12:15:47 +0300

    fix typo

--- backend/main.js
+++ backend/main.js
@@ -6,14 +6,14 @@ const app = express()
 
 const router_user = require("./Router/user_router")
 const router_resume = require("./Router/extract_resume_router")
-const create_tset = require("./Router/level_exam_router")
+const create_test = require("./Router/level_exam_router")
 
 
 app.use(cors())
 app.use(express.json())
 app.use("/user", router_user)
 app.use("/resume", router_resume)
-app.use("/test", create_tset)
+app.use("/test", create_test)
 
 const port = process.env["PORT"] || 8000;
 app.listen(port,
@@ -21,4 +21,4 @@ app.listen(port,
 
 mongoose.connect("mongodb://127.0.0.1:27017/programmer_guide")
     .then(() => console.log("Connected to MongoDB"))
-	.catch(err => console.error("Failed to connect to MongoDB", err));
+    .catch(err => console.error("Failed to connect to MongoDB", err));
