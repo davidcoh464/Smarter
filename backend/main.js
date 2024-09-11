2c23380bae9bf230215ded1fa6280c9cbc4ef460
David Cohen <deiv464@gmail.com>
2024-09-12 00:24:08 +0300

    add study route

--- backend/main.js
+++ backend/main.js
@@ -9,6 +9,7 @@ const router_resume = require("./Router/extract_resume_router");
 const create_test = require("./Router/level_exam_router");
 const job_match_router = require("./Router/job_match_router");
 const project_router = require("./Router/project_idea_router");
+const tool_learning = require("./Router/tool_learning_router");
 
 app.use(cors());
 app.use(express.json());
@@ -17,6 +18,7 @@ app.use("/resume", router_resume);
 app.use("/test", create_test);
 app.use("/job_match", job_match_router);
 app.use("/project", project_router);
+app.use("/study", tool_learning);
 
 const port = process.env["PORT"] || 8000;
 app.listen(port, () => {
